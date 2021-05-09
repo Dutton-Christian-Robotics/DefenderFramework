9d9f6d9f591ced417bc0b4100e4ab5a7c472afb8
Matthew Roberts <thew.roberts@gmail.com>
2021-05-09 06:02:51 +0000

    Bot classes.

--- DefenderBot/DefenderBotCanNavigateToPosition.java
+++ DefenderBot/DefenderBotCanNavigateToPosition.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode.dcs15815;
 
 import java.util.ArrayList;
 
-public interface DefenderCanNavigateToPosition {
+public interface DefenderBotCanNavigateToPosition {
     public void driveToPosition(double x, double y);
     public void driveToPosition(DefenderBotPosition position);
 }
